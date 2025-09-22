5ab711a2ea41c727e30052c16c810b687edc086e
Ksh Siaan <kshsiaan@gmail.com>
2025-09-22 12:26:51 +0600

    quick

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/next";
+
 export const metadata: Metadata = {
   title: "The Screaming Parrots Cafe – Desserts, Tea & Bites in Redmond, WA",
   description:
@@ -38,7 +40,29 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P2LFJ3RG');`,
+          }}
+        />
+      </head>
       <body className={`antialiased overflow-x-hidden !scroll-smooth`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P2LFJ3RG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
